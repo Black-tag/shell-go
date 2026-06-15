fd58dffd7a2113e35a738a1a96753ade79f230e7
Black-tag <unnianandunni007@gmail.com>
2026-06-15 17:42:19 +0000

    codecrafters submit [skip ci]

--- app/internal/shell/jobs.go
+++ app/internal/shell/jobs.go
@@ -17,10 +17,10 @@ type Job struct {
 func (s *Shell)jobs(args []string) {
 	for _, job := range s.Jobs {
 		fmt.Printf(
-			"[%d]+  %-24s%s%s\n",
+			"[%d]+  %-24s%s\n",
 			job.ID,
 			job.Status,
-			job.Command
+			job.Command,
 		)
 	}
 }
