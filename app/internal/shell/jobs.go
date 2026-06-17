ad9efe12f1492c4a7b27e919d712896121732f38
Black-tag <unnianandunni007@gmail.com>
2026-06-17 17:11:35 +0000

    codecrafters submit [skip ci]

--- app/internal/shell/jobs.go
+++ app/internal/shell/jobs.go
@@ -50,50 +50,65 @@ func (s *Shell) Job() {
 
 }
 
-func (s *Shell) ReapJobs() {
+// func (s *Shell) ReapJobs() {
 
-	var doneIndexes []int
+// 	var doneIndexes []int
 	// jobCount := len(s.Jobs)
 	// fmt.Println("before reap:", len(s.Jobs))
 
-	for i, job := range s.Jobs {
-		if job.Status == "Done" {
-			doneIndexes = append(doneIndexes, i)
-			switch i {
-			// case jobCount - 1:
-			// 	fmt.Printf(
-			// 		"[%d]+  %-24s%s\n",
-			// 		job.ID,
-			// 		job.Status,
-			// 		job.Command,
-			// 	)
+// 	for i, job := range s.Jobs {
+// 		if job.Status == "Done" {
+// 			doneIndexes = append(doneIndexes, i)
+// 			// switch i {
+// 			// case jobCount - 1:
+// 			// 	fmt.Printf(
+// 			// 		"[%d]+  %-24s%s\n",
+// 			// 		job.ID,
+// 			// 		job.Status,
+// 			// 		job.Command,
+// 			// 	)
 
-			// case jobCount - 2:
-			// 	fmt.Printf(
-			// 		"[%d]-  %-24s%s\n",
-			// 		job.ID,
-			// 		job.Status,
-			// 		job.Command,
-			// 	)
+// 			// case jobCount - 2:
+// 			// 	fmt.Printf(
+// 			// 		"[%d]-  %-24s%s\n",
+// 			// 		job.ID,
+// 			// 		job.Status,
+// 			// 		job.Command,
+// 			// 	)
 
-			default:
-				fmt.Printf(
-					"[%d]  %-24s%s\n",
-					job.ID,
-					job.Status,
-					job.Command,
-				)
+// 			// default:
+// 			// 	fmt.Printf(
+// 			// 		"[%d]  %-24s%s\n",
+// 			// 		job.ID,
+// 			// 		job.Status,
+// 			// 		job.Command,
+// 			// 	)
 
-			}
+// 			}
 
-		}
+// 		}
 
-	}
-	// fmt.Println("removing:", doneIndexes)
-	for i := len(doneIndexes) - 1; i >= 0; i-- {
-		idx := doneIndexes[i]
-		s.Jobs = append(s.Jobs[:idx], s.Jobs[idx+1:]...)
-	}
-	// fmt.Println("after reap:", len(s.Jobs))
+// 	}
+// 	// fmt.Println("removing:", doneIndexes)
+// 	for i := len(doneIndexes) - 1; i >= 0; i-- {
+// 		idx := doneIndexes[i]
+// 		s.Jobs = append(s.Jobs[:idx], s.Jobs[idx+1:]...)
+// 	}
+// 	// fmt.Println("after reap:", len(s.Jobs))
 
+// }
+
+func (s *Shell) ReapJobs() {
+    var doneIndexes []int
+
+    for i, job := range s.Jobs {
+        if job.Status == "Done" {
+            doneIndexes = append(doneIndexes, i)
+        }
+    }
+
+    for i := len(doneIndexes)-1; i >= 0; i-- {
+        idx := doneIndexes[i]
+        s.Jobs = append(s.Jobs[:idx], s.Jobs[idx+1:]...)
+    }
 }
